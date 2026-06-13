64cddd60bdde482eff98b606023f2b4a3956ff95
ESM-6003 <inscripcionesesm@gmail.com>
2026-06-13 11:41:36 -0300

    Delete web_config.js

--- web_config.js
+++
@@ -1,8 +0,0 @@
-window.WEB_CONFIG = {
-  // Pegar aqui la URL de implementacion Web App de Google Apps Script.
-  // Ejemplo: "https://script.google.com/macros/s/AKfycb.../exec"
-  sheetsAppendUrl: "https://script.google.com/macros/s/AKfycbxiSgrPRKrFoiraRX0K0cFpzTboRxmeCKSugeML9ULPmO42_fGNG3JOlLxSNnkWyn2g/exec",
-
-  // Opcional. Si lo usas, tambien configuralo en el Apps Script.
-  appSecret: ""
-};
